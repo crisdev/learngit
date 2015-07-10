ebbda7d158c4a4c07539bd6974339c939e5f6d5c
Cristian Vincenzini <martindeneuquen@gmail.com>
2015-07-10 17:18:34 -0300

    Improving C code

--- new.c
+++ new.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+/**
+ * This program takes a number and informs the user if it's positive, negative or zero.
+ *
+ * @version 10-07-2015
+ */
 int main(void)
 {
-	printf("hola mundo\n");
+	int number;
 
-	return 0;
+	printf("Please enter a number: ");
+	scanf("%d", &number);
+
+// checking stuff
+	if (number > 0)
+		printf("Number is greater than zero.\n");
+	else if (number < 0)
+		printf("Number is negative.\n");
+	else
+		printf("Number is zero.\n");
+
+	return EXIT_SUCCESS;
 }
